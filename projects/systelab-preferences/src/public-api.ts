04e3500ea3d7da0bfaaf9c76b70cac4cb6551091
alfonsserra <alfonsoserra@gmail.com>
2020-03-14 09:20:43 +0100

    Use the cli approach for the library

---
+++ projects/systelab-preferences/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of systelab-preferences
+ */
+
+export * from './lib/local-storage.service';
+export * from './lib/memory-storage.service';
+export * from './lib/session-storage.service';
+export * from './lib/preferences.service';
+export * from './lib/systelab-preferences.module';
